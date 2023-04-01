81a69365f5eb457e667c1813d607e57b2acf0635
XIAO0509 <liuxiaoxiao2089@gmail.com>
2023-04-01 21:24:02 +0300

    решение

--- system.js
+++ system.js
@@ -1,4 +1,23 @@
+function printResult(text){
+    document.getElementById('result').innerHTML=text
+}
 function solveSystemEq(){
-    let a=document.getElementById('a').a;
-    let b=document.getElementById('b').b;
+    let a=document.getElementById('a').value;
+    let b=document.getElementById('b').value;
+    let x=document.getElementById('x').value;
+    a=+a;
+    b=+b;
+    x=+x;
+    console.log('a',a,'b',b,'x',x);
+    if(a==NaN||b==NaN||x==NaN){
+        alert('ОШИБКА!');
+    }
+    else if(x>=7.3){
+        y=x/b-a*x;
+        printResult('y='+y);
+    }
+    else if(x<7.3){
+        y=x/a-b*x;
+        printResult('y='+y);
+    }
 }
